5290a1c73cb27fbca3d4f3f606a3a002ef859c4d
matthew.serre <matthew.serre@gmail.com>
2018-11-14 03:21:37 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,10 +21,6 @@ class Transfer
     @sender.balance = @sender.balance - @amount
     @receiver.balance = @receiver.balance + @amount
     "complete"
-    
-          expect(amanda.balance).to eq(950)
-      expect(avi.balance).to eq(1050)
-      expect(transfer.status).to eq("complete")
   end
   
 end
