309ee88c7190f435552061549111a684aad6e808
matthew.serre <matthew.serre@gmail.com>
2018-11-14 03:42:29 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if @status == "complete" && @sender.balance >= @amount && @sender.status == "open"
+    if @status == "complete" && @receiver.balance >= @amount && @sender.status == "open"
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "reversed"
