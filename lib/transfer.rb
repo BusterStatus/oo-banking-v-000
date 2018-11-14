3d00d39e7e3aecaacc3e984b4e875d45d358a3f5
matthew.serre <matthew.serre@gmail.com>
2018-11-14 03:21:52 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
   def execute_transaction
     @sender.balance = @sender.balance - @amount
     @receiver.balance = @receiver.balance + @amount
-    "complete"
+    @status = "complete"
   end
   
 end
